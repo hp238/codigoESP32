336dbf582eb8158e2193717714795a4946d80872
hp238 <59709400+hp238@users.noreply.github.com>
2020-08-31 19:28:59 -0300

    Add files via upload

--- codigoESP32.ino
+++ codigoESP32.ino
@@ -6,40 +6,44 @@ BluetoothSerial SerialBT;
 //pinagem do ESP32
 //#define pinDIN 12     //MISO
 //#define pinDOUT 13    //MOSI
-#define pinCS 3       //CS
-#define pinSCLK 14    //SCLK
+#define pinCS    3      //CS
+#define pinSCLK 14      //SCLK
 #define pinSTART 4
 #define pinRESET 2
 #define pinPWDN 22
 #define pinDRDY 16
-#define pinCLK 6
+#define pinCLK   6
 
 //comandos ADS
-uint8_t SDATAC = 0x11;
-uint8_t START = 0X08;
-uint8_t STOP = 0x0A;
-uint8_t WREG = 0x40;
-uint8_t RREG = 0x20;
+uint8_t SDATAC =  0x11;
+uint8_t START  =  0X08;
+uint8_t STOP   =  0x0A;
+uint8_t WREG   =  0x40;
+uint8_t RREG   =  0x20;
+uint8_t RDATA  =  0X12;
 
 //Registradores
 uint8_t CONFIG1 = 0x01;
 uint8_t CONFIG2 = 0x02;
 uint8_t CONFIG3 = 0x03;
-uint8_t LOFF = 0x04;
-uint8_t CH1SET = 0x05;
-uint8_t CH2SET = 0x06;
-uint8_t CH3SET = 0x07;
-uint8_t CH4SET = 0x08;
+uint8_t LOFF    = 0x04;
+uint8_t CH1SET  = 0x05;
+uint8_t CH2SET  = 0x06;
+uint8_t CH3SET  = 0x07;
+uint8_t CH4SET  = 0x08;
 uint8_t BIAS_SENSP = 0x0D;
 uint8_t BIAS_SENSN = 0x0E;
 uint8_t LOFF_SENSP = 0x0F;
 uint8_t LOFF_SENSN = 0x10;
-uint8_t LOFF_FLIP = 0x11;
+uint8_t LOFF_FLIP  = 0x11;
 uint8_t LOFF_STATP = 0x12;
 uint8_t LOFF_STATN = 0x13;
 //uint8_t GPIO = 0x14;
-uint8_t MISC1 = 0x15;
-uint8_t MISC2 = 0x16;
+uint8_t MISC1  = 0x15;
+uint8_t MISC2  = 0x16;
+
+//Variáveis
+int32_t ecgSignal[4];
 
 /**************************************************************************************************
  * Inicialização
@@ -47,22 +51,22 @@ uint8_t MISC2 = 0x16;
  
 //inicialização das portas do ESP
 void gpioStartup() {
-  //pinMode(pinDIN, INPUT);        //entrada serial 
-  //pinMode(pinDOUT, OUTPUT);      //saída serial
-  pinMode(pinCS, OUTPUT);        //LOW p/ ativar comunicação SPI
-  pinMode(pinSCLK, OUTPUT);      //entrada do clock da comunicação SPI
-  pinMode(pinSTART, OUTPUT);     //HIGH p/ iniciar conversão
+  //pinMode(pinDIN, INPUT);       //entrada serial 
+  //pinMode(pinDOUT, OUTPUT);     //saída serial
+  pinMode(pinCS,    OUTPUT);      //LOW p/ ativar comunicação SPI
+  pinMode(pinSCLK,  OUTPUT);      //entrada do clock da comunicação SPI
+  pinMode(pinSTART, OUTPUT);      //HIGH p/ iniciar conversão
                                   //LOW p/ para conversão (ou STOP)
   pinMode(pinRESET, OUTPUT);
-  pinMode(pinPWDN, OUTPUT); 
-  pinMode(pinDRDY, INPUT);       //HIGH durante a conversão dos dados
+  pinMode(pinPWDN,  OUTPUT); 
+  pinMode(pinDRDY,  INPUT);       //HIGH durante a conversão dos dados
                                   //LOW p/ informar a existência de dados disponíveis p/ leitura  
-  pinMode(pinSCLK, OUTPUT);       //entrada do clock
+  pinMode(pinSCLK,  OUTPUT);      //entrada do clock
   
-  digitalWrite(pinRESET, HIGH);  //LOW p/ resetar
-  digitalWrite(pinPWDN, HIGH);   //alimentar os circuitos
-  digitalWrite(pinSTART, LOW);   
-  digitalWrite(pinCS, HIGH);     //desativar comunicação SPI
+  digitalWrite(pinRESET,  HIGH);  //LOW p/ resetar
+  digitalWrite(pinPWDN,   HIGH);  //alimentar os circuitos
+  digitalWrite(pinSTART,  LOW);   
+  digitalWrite(pinCS,     HIGH);  //desativar comunicação SPI
   
 }
 
@@ -205,6 +209,23 @@ void spiStartup() {
   digitalWrite(pinCS, HIGH);            //desativar comunicação SPI
   return dat;
  }
+
+ //ler os dados do ADS (24 bits)
+ void readData(){
+  digitalWrite(pinCS, LOW);             //ativar comunicação SPI
+  SPI.transfer(RDATA);
+
+  for(int i = 0; i < 4; i++){
+    ecgSignal[i] = ((uint32_t)SPI.transfer(0)) << 16;  //le um byte para as 8 primeiras casas, desloca para a 16a
+    ecgSignal[i] |= ((uint32_t)SPI.transfer(0)) << 8;  //le outro byte, desloca
+    ecgSignal[i] |= ((uint32_t)SPI.transfer(0)) << 0; //le o 3o byte e sao todos salvos na mesma variavel
+    if (ecgSignal[i] & (1UL << 23))ecgSignal[i] |= 0xFF000000UL; //1UL<<23= 1 bit unsigned long deslocado 23 casas
+  }
+
+  digitalWrite(pinCS, HIGH);            //desativar comunicação SPI
+ }
+
+ 
 //
 /**************************************************************************************************
  * SETUP e LOOP
@@ -235,11 +256,11 @@ void setup()
     
 void loop()
 {
-   if (Serial.available()) {
-    SerialBT.write(Serial.read());
-  }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
-  }
-  delay(20);
+   while (digitalRead(pinDRDY));
+   
+   readData();
+
+   for(int i=1 ; i<=4 ; i++){
+    Serial.println(ecgSignal[i]);
+   }
 }
